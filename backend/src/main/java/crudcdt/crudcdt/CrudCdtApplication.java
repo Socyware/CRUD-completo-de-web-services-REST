83309e331ca2b3ee4f357133563f5ac6f6f49365
Alan <socyware@gmail.com>
2022-11-14 13:35:35 -0300

    Entities

---
+++ backend/src/main/java/crudcdt/crudcdt/CrudCdtApplication.java
@@ -0,0 +1,13 @@
+package crudcdt.crudcdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCdtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/crudcdt/crudcdt/CrudCdtApplicationTests.java
@@ -0,0 +1,13 @@
+package crudcdt.crudcdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
